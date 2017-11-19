a86f4c5c2e4c0dbce9ca7d63b947d5a359b75a72
leonidas56 <leo1996l@hotmail.com>
2017-11-19 15:57:50 -0300

    Update GestorCliente.java

--- WebApplication1/src/java/Controladores/GestorCliente.java
+++ WebApplication1/src/java/Controladores/GestorCliente.java
@@ -5,25 +5,75 @@
  */
 package Controladores;
 
+import Model.Alquiler;
 import Model.Cliente;
-import java.util.ArrayList;
+import Model.Puesto;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author Yasmin
- */
+
 public class GestorCliente {
 
-    public ArrayList<Cliente> obtenerClientes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private Connection conn;
+
+    public GestorCliente() {
+        AccesoDatos ad = new AccesoDatos();
+        try {
+            conn = DriverManager.getConnection(ad.getConn_string(), ad.getUser(), ad.getPass());
+        } catch (SQLException ex) {
+            Logger.getLogger(GestorCliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Cliente obtenerCliente(int id) {
+        
+        Cliente c = new Cliente();
+        try {
+            PreparedStatement stmt = conn.prepareStatement("exec pa_obtener_alquiler ?");
+            stmt.setInt(1, id);
+            ResultSet query = stmt.executeQuery();
+            if(query.next()){            
+                c.setApellido(query.getString("apellido"));
+                c.setNombre(query.getString("nombre"));
+                c.setIdTipoDocumento(query.getInt("id_tipo_documento"));
+                c.setDocumento(query.getInt("documento"));
+                c.setIdCliente(query.getInt("id_cliente"));
+                c.setTelefono(query.getInt("telefono"));                      
+                
+            }
+            query.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(GestorCliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return c;
     }
 
     public boolean agregarCliente(Cliente c) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        boolean inserto = true;
+        try {
+            PreparedStatement stmt = conn.prepareStatement("EXEC pa_insert_alquiler ?, ?, ?, ?, ?");
+            stmt.setString(1, c.getNombre());
+            stmt.setString(2, c.getApellido());
+            stmt.setInt(3, c.getDocumento());
+            stmt.setInt(4, c.getIdTipoDocumento());
+            stmt.setLong(5, c.getTelefono());
+            stmt.executeUpdate();
+            stmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(GestorCliente.class.getName()).log(Level.SEVERE, null, ex);
+            inserto = false;
+        }
+        return inserto;
     }
 
-    Cliente obtenerCliente() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
